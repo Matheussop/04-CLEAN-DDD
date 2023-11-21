b41bb45812b248e7a7aebab3b1aadc51a5000aaa
Matheus Luiz Oliveira Spindula <matheussopluiz@outlook.com>
2023-11-21 18:26:02 -0300

    test: List answers by question

---
+++ src/domain/forum/application/use-cases/list-question-answers.spec.ts
@@ -0,0 +1,60 @@
+import { makeAnswer } from 'test/factories/make-answer'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { ListQuestionAnswersUseCase } from './list-question-answers'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { makeQuestion } from 'test/factories/make-question'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let sut: ListQuestionAnswersUseCase
+
+describe('List Question Answers', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new ListQuestionAnswersUseCase(inMemoryAnswersRepository)
+  })
+
+  it('should be able to fetch question answers', async () => {
+    await inMemoryAnswersRepository.create(
+      makeAnswer({
+        questionId: new UniqueEntityID('question-1'),
+      }),
+    )
+    await inMemoryAnswersRepository.create(
+      makeAnswer({
+        questionId: new UniqueEntityID('question-1'),
+      }),
+    )
+    await inMemoryAnswersRepository.create(
+      makeAnswer({
+        questionId: new UniqueEntityID('question-1'),
+      }),
+    )
+
+    const { answers } = await sut.execute({
+      questionId: 'question-1',
+      page: 1,
+    })
+
+    expect(answers).toHaveLength(3)
+  })
+
+  it('should be able to fetch paginated question answers', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await inMemoryAnswersRepository.create(
+        makeAnswer({
+          questionId: new UniqueEntityID('question-1'),
+        }),
+      )
+    }
+
+    const { answers } = await sut.execute({
+      questionId: 'question-1',
+      page: 2,
+    })
+
+    expect(answers).toHaveLength(2)
+  })
+})
